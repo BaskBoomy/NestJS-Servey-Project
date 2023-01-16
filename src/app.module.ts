cd6689dca4c1bc81654940369ee5ca8d79e132ce
jack <whdgurtpqms@gmail.com>
2023-01-16 14:15:19 +0900

    server setup

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppResolver } from './app.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      definitions:{
+        path: join(process.cwd(), 'src/graphql.ts'),
+      }
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'survey_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize:true,
+    })
+  ],
+  controllers: [],
+  providers: [AppResolver],
+})
+export class AppModule {}
